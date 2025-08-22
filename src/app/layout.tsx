bd8ad520c357fa8680be6d9839a3cf8272bd77c4
YeongTaek <yeongtaek2109@gmail.com>
2025-08-23 08:30:27 +0900

    refactor: TanStack Query layout 분리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,18 @@
-'use client';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Providers from './providers';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { useState } from 'react';
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 5 * 60 * 1000,
-            refetchOnWindowFocus: false,
-          },
-        },
-      })
-  );
-
   return (
     <html lang="ko">
-      <body>
-        <QueryClientProvider client={queryClient}>
-          {children}
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
