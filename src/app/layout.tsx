7902ccb80ee7bf0f620b464df627cffb8cd5ee21
YeongTaek <yeongtaek2109@gmail.com>
2025-09-16 22:10:30 +0900

    remove: 목표 도메인 초기화

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,14 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Providers from "./providers";
-import Header from "@/components/layout/Header";
-import TransactionAlertProvider from "@/components/budget/TransactionAlertProvider";
-const inter = Inter({ subsets: ["latin"] });
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Providers from './providers';
+import Header from '@/components/layout/Header';
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "DaylyLog - 매일의 기록이 만드는 특별한 변화",
+  title: 'DaylyLog - 매일의 기록이 만드는 특별한 변화',
   description:
-    "일상을 기록하고, 목표를 달성하며, 성장을 추적하는 스마트한 방법",
-  keywords: "가계부, 목표달성, 일상기록, 성찰, 자기계발",
+    '일상을 기록하고, 목표를 달성하며, 성장을 추적하는 스마트한 방법',
+  keywords: '가계부, 목표달성, 일상기록, 성찰, 자기계발',
 };
 
 export default function RootLayout({
@@ -25,13 +24,11 @@ export default function RootLayout({
       </head>
       <body className={`${inter.className} bg-gray-50 min-h-screen`}>
         <Providers>
-          <TransactionAlertProvider>
-            <div className="min-h-screen flex flex-col">
-              <Header />
-              <main className="flex-1">{children}</main>
-              {/* // TODO: Footer 추가 */}
-            </div>
-          </TransactionAlertProvider>
+          <div className="min-h-screen flex flex-col">
+            <Header />
+            <main className="flex-1">{children}</main>
+            {/* // TODO: Footer 추가 */}
+          </div>
         </Providers>
       </body>
     </html>
